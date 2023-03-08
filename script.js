155ac602caeaeb5755594656bf462dcab3681f18
xXMemeMachineXx <101399351+xXMemeMachineXx@users.noreply.github.com>
2023-03-08 21:04:30 +0100

    Shhh

--- script.js
+++ script.js
@@ -26,8 +26,33 @@
       document.title = `Blinkendelys.no blink: ${teller}`
     }
   }
+
+  function BlinkeLysDisco() {
+    const lys = document.getElementById("LightOff")
+    farge.randomColor = Math.floor(Math.random()*16777215).toString(16);
+    lys.style.backgroundColor = "#" + farge.randomColor;
+    
+    if (current_state == 0) {
+      const lys = document.getElementById("LightOff")
+      lys.style.backgroundColor = "white";
+      current_state = 1;
+    } else {
+      const lys = document.getElementById("LightOff")
+      lys.style.backgroundColor = "#" + farge.randomColor;
+      current_state = 0;
+      teller = teller+1
+      document.title = `Blinkendelys.no blink: ${teller}`
+    }
+  }
   
   function timer() {
     setInterval(BlinkeLys, 100);
+    check = check + 1
+  }
+
+  function timerDisco() {
+    if(check < 1){
+      setInterval(BlinkeLysDisco, 100);
+    }
     check = check + 1
   }
