cb8c2ab36fb62b9598e3f3c30818326cf8a19401
xXMemeMachineXx <101399351+xXMemeMachineXx@users.noreply.github.com>
2023-06-17 12:53:21 +0200

    Disco Mode Added

--- script.js
+++ script.js
@@ -2,18 +2,34 @@
   var current_state = 0;
   var teller = 0;
   var check = 0;
+  var modeNumber = 0;
   var farge = {};
+  var NumberOfModes = 1; //antall moduser -1
+
+  const modusNavnListe = ["Original", "Disco Mode", "Custom Color"]
+  var modusNavn;
   
   function start(){
     const lys = document.getElementById("LightOff")
     farge.randomColor = Math.floor(Math.random()*16777215).toString(16);
-    lys.style.backgroundColor = "#" + farge.randomColor;
-    if(check < 1){
-      timer()
-    }
+    lys.style.backgroundColor = "#" + farge.randomColor
+      if(modeNumber == 0){
+        check = 1;
+        timer()
+      } else if(modeNumber == 1){
+        check = 2;
+        timerDisco();
+      }
   }
 
+
+
+
+
+
+
   function BlinkeLys() {
+    if(check == 1){
     if (current_state == 0) {
       const lys = document.getElementById("LightOff")
       lys.style.backgroundColor = "white";
@@ -26,8 +42,10 @@
       document.title = `Blinkendelys.no blink: ${teller}`
     }
   }
+  }
 
   function BlinkeLysDisco() {
+    if(check == 2){
     const lys = document.getElementById("LightOff")
     farge.randomColor = Math.floor(Math.random()*16777215).toString(16);
     lys.style.backgroundColor = "#" + farge.randomColor;
@@ -44,15 +62,49 @@
       document.title = `Blinkendelys.no blink: ${teller}`
     }
   }
+  }
   
+
+
+
+
+
+
+
+
+
+
   function timer() {
+    if(check == 1){
     setInterval(BlinkeLys, 100);
-    check = check + 1
+    }
   }
 
   function timerDisco() {
-    if(check < 1){
-      setInterval(BlinkeLysDisco, 100);
+    if(check == 2){
+      setInterval(BlinkeLysDisco, 75);
     }
-    check = check + 1
+  }
+
+
+  function goRight(){
+    if(modeNumber==NumberOfModes){   
+      modeNumber=0;
+    } else{
+      modeNumber=modeNumber+1;
+    }
+
+    ModeName.innerText = modusNavnListe[modeNumber].toLocaleString('en-US')
+    console.log(modeNumber);
+  }
+
+  function goLeft(){
+    if(modeNumber==0){
+      modeNumber=NumberOfModes;
+    } else{
+      modeNumber=modeNumber-1;
+    }
+
+    ModeName.innerText = modusNavnListe[modeNumber].toLocaleString('en-US')
+    console.log(modeNumber);
   }
