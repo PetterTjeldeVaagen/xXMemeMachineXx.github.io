9109abe8f04dad64d6af6635d94e37f362b52b80
xXMemeMachineXx <101399351+xXMemeMachineXx@users.noreply.github.com>
2023-06-18 20:18:44 +0200

    CustomColorMode

--- script.js
+++ script.js
@@ -4,21 +4,44 @@
   var check = 0;
   var modeNumber = 0;
   var farge = {};
-  var NumberOfModes = 1; //antall moduser -1
+  var NumberOfModes = 2; //antall moduser -1
+
+  var red=1;
+  var blue=1;
+  var green=1;
+
+  var customColor;
+
+  var RedSlider = document.getElementById("RedSlider");
+  var RedOutput = document.getElementById("redNumber");
+  RedOutput.innerHTML = RedSlider.value;
+
+  var GreenSlider = document.getElementById("GreenSlider");
+  var GreenOutput = document.getElementById("greenNumber");
+  GreenOutput.innerHTML = GreenSlider.value;
+
+  var BlueSlider = document.getElementById("BlueSlider");
+  var BlueOutput = document.getElementById("blueNumber");
+  BlueOutput.innerHTML = BlueSlider.value;
+
+  var ColorPicker = document.getElementById("ColorPicker");
 
   const modusNavnListe = ["Original", "Disco Mode", "Custom Color"]
   var modusNavn;
   
   function start(){
-    const lys = document.getElementById("LightOff")
-    farge.randomColor = Math.floor(Math.random()*16777215).toString(16);
-    lys.style.backgroundColor = "#" + farge.randomColor
+  const lys = document.getElementById("LightOff")
+  farge.randomColor = Math.floor(Math.random()*16777215).toString(16);
       if(modeNumber == 0){
         check = 1;
         timer()
       } else if(modeNumber == 1){
         check = 2;
         timerDisco();
+
+      } else if(modeNumber == 2){
+        check = 3;
+        timerCustom();
       }
   }
 
@@ -37,6 +60,22 @@
     } else {
       const lys = document.getElementById("LightOff")
       lys.style.backgroundColor = "#" + farge.randomColor;
+      current_state = 0;
+      teller = teller+1
+      document.title = `Blinkendelys.no blink: ${teller}`
+    }
+  }
+  }
+
+  function BlinkeLysCustom() {
+    if(check == 3){
+    if (current_state == 0) {
+      const lys = document.getElementById("LightOff")
+      lys.style.backgroundColor = "white";
+      current_state = 1;
+    } else {
+      const lys = document.getElementById("LightOff")
+      lys.style.backgroundColor = customColor ;
       current_state = 0;
       teller = teller+1
       document.title = `Blinkendelys.no blink: ${teller}`
@@ -64,14 +103,32 @@
   }
   }
   
+  
 
+  RedSlider.oninput = function() {
+    RedOutput.innerHTML = this.value;
+    red=this.value;
+    fargeLys();
+  }
 
+  GreenSlider.oninput = function() {
+    GreenOutput.innerHTML = this.value;
+    green=this.value;
+    fargeLys();
+  }
 
+  BlueSlider.oninput = function() {
+    BlueOutput.innerHTML = this.value;
+    blue=this.value;
+    fargeLys();
+  }
 
-
-
-
-
+  function fargeLys(){
+    const lys = document.getElementById("LightOff")
+    customColor = "rgba("+red+","+ green+ "," + blue + ")";
+    lys.style.backgroundColor= customColor;
+    console.log(customColor);
+  }
 
 
   function timer() {
@@ -86,6 +143,12 @@
     }
   }
 
+  function timerCustom() {
+    if(check == 3){
+    setInterval(BlinkeLysCustom, 100);
+    }
+  }
+
 
   function goRight(){
     if(modeNumber==NumberOfModes){   
@@ -93,18 +156,31 @@
     } else{
       modeNumber=modeNumber+1;
     }
-
+    checkForStuff()
     ModeName.innerText = modusNavnListe[modeNumber].toLocaleString('en-US')
     console.log(modeNumber);
+    
   }
 
   function goLeft(){
+    
     if(modeNumber==0){
       modeNumber=NumberOfModes;
     } else{
       modeNumber=modeNumber-1;
     }
-
+    checkForStuff()
     ModeName.innerText = modusNavnListe[modeNumber].toLocaleString('en-US')
     console.log(modeNumber);
+  }
+
+  function checkForStuff(){
+    if(modeNumber == 0){
+      ColorPicker.style.display = "none";
+    } else if(modeNumber == 1){
+      ColorPicker.style.display = "none";
+    } else if(modeNumber == 2){
+      ColorPicker.style.display = "block";
+    }
+    
   }
